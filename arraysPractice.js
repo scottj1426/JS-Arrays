e55c45cd2ed4d61ab11cac4924f7d6ca42521fc5
James Scott <scottj1426@gmail.com>
2017-08-28 14:35:15 -0500

    Project 2 Day 6

--- arraysPractice.js
+++ arraysPractice.js
@@ -5,6 +5,9 @@ var arr = [10,20,30];
 //Create a function named 'first' that is given 'arr' as the argument and  returns the first item in the given array.
 
   //Code Here
+  function first(arr){
+    return arr[0];
+  }
 
 
 //Next problem
@@ -16,6 +19,9 @@ var arr = [40,50,60];
 
 
   //Code Here
+  function last(arr){
+    return arr[arr.length-1];
+  }
 
 
 //Next Problem
@@ -25,6 +31,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 //Create a function named 'looper' that is given family as it's only argument, loops through the given array, and alerts every item in the array.
 
   //Code Here
+  function looper(family){
+    for(var i = 0; i < family.length;i++){
+      alert(family[i]);
+    }
+  }
 
 
 //Next problem
@@ -35,6 +46,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 //Write a function called reversedLooper that is given letters as it's only argument and loops through the given array backwards alerting every item in the array starting at the end.
 
   //Code Here
+  function reversedLooper(letters){
+    for(var i = letters.length-1; i >= 0; i--){
+    alert(letters[i]);
+    }
+      }
 
 
 //Next Problem
@@ -44,11 +60,20 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 //Write a function named evenFinder that is given nums as it's only argument and removes all values that aren't even from the given array.
 
   //Code Here
+  var nums = [1,2,3,4,5,6,7,8,9];
+function evenFinder(nums){
+    for(var i = 0;i < nums.length;i++){
+      if(nums[i] % 2 !== 0){
+        nums.splice(i,1);
+      }
+    }
+    return nums;
+  }
 
 
 
 
-  
+
 
 
 
@@ -73,6 +98,20 @@ var numbersArray = [1,2,34,54,55,34,32,11,19,17,54,66,13];
 
 
   //Code Here
+  var numbersArray = [1,2,34,54,55,34,32,11,19,17,54,66,13];
+var odds = [];
+  var evens = [];
+  function divider(numbersArray){
+    for(var i =0; i < numbersArray.length;i++){
+      if(numbersArray[i] % 2 === 0){
+        evens.push(numbersArray[i]);
+      }
+      else if(numbersArray[i] % 2 === 1){
+        odds.push(numbersArray[i]);
+      }
+    }
+    return [evens,odds];
+  }
 
 
 //Next Problem
@@ -87,7 +126,12 @@ var getRandomArbitrary = function() {
 // Your job is to write a function named finder that will get a random number (by invoking getRandomArbitrary), then loop through the array (that will be passed in as a parameter) to see if that random number is in the array. If it is, return true, if it's not, return false
 
   //Code Here
+function finder(numbers){
+  getRandomArbitrary(numbers);
+  for(var i = 0; i < numbers.length){
+  }
 
+}
 
 
 
